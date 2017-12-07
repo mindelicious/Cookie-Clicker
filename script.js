c0e31d075ec7b56422fa1009e127697360e084d7
Matt <[mailprogramisty@gmail.com]>
2017-12-07 21:55:34 +0100

    add some improv

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ saveScoreElm.onclick = function() {
     localStorage.setItem('cookieCount', cookieCount);
     localStorage.setItem('autoClick', autoClick);
     localStorage.setItem('grandma', grandma);
+    localStorage.setItem('farm', farm);
+    localStorage.setItem('bakery', bakery);
+    localStorage.setItem('mine', mine);
 }
 
 loadScoreElm.onclick = function() {
@@ -28,17 +31,34 @@ loadScoreElm.onclick = function() {
     autoClick = parseInt(autoClick);
     grandma = localStorage.getItem('grandma');
     grandma = parseInt(grandma);
+    farm = localStorage.getItem('farm');
+    farm = parseInt(farm);
+    bakery = localStorage.getItem('bakery');
+    bakery = parseInt(bakery);
+    mine = localStorage.getItem('mine');
+    mine = parseInt(mine);
     update();
 }
 
 //Update
 function update() {
-    score.innerHTML = cookieCount.toFixed(2);
+    score.innerHTML = cookieCount;
+
     document.getElementById('amountClick').innerHTML = 'Amount: ' + autoClick;
     document.getElementById('costClick').innerHTML = 'For ' + ((autoClick+1) * 3) + ' Cookies';
+  
     document.getElementById('amountGrandma').innerHTML = 'Amount: ' + grandma;
-    document.getElementById('costGrandma').innerHTML ='For ' + ((grandma+1) * 15) + ' Cookies';
-    cookiePerSec.innerHTML = 'Per second: ' + (((autoClick)+(grandma * 2)) * multiplier).toFixed(2);
+    document.getElementById('costGrandma').innerHTML = 'For ' + ((grandma+1) * 15) + ' Cookies';
+    cookiePerSec.innerHTML = (((autoClick)+(grandma * 2)+(farm * 3)+(bakery * 4)+(mine * 6)) * multiplier).toFixed(2);
+    
+    document.getElementById('amountFarm').innerHTML = 'Amount: ' + farm;
+    document.getElementById('costFarm').innerHTML = 'For ' + ((farm+1) * 30) + ' Cookies';
+
+    document.getElementById('amountBakery').innerHTML = 'Amount: ' + bakery;
+    document.getElementById('costBakery').innerHTML = 'For ' + ((bakery+1) * 50) + ' Cookies';
+
+    document.getElementById('amountMine').innerHTML = 'Amount: ' + mine;
+    document.getElementById('costMine').innerHTML = 'For ' + ((mine+1) * 80) + ' Cookies';
 }
 
 // Add timer/ buy autoClick
@@ -48,12 +68,15 @@ var autoClick = 0;
 function timer() {
     cookieCount = cookieCount + autoClick;
     cookieCount = cookieCount + grandma*2;
+    cookieCount = cookieCount + farm*3;
+    cookieCount = cookieCount + bakery*5;
+    cookieCount = cookieCount + mine*7;
     update()
 }
 setInterval(timer, 2000)
 
 buyClick.onclick = function buyClick() {
-    if(cookieCount >= ((autoClick+1) * 3)) {
+    if(cookieCount >= (autoClick+1) * 3) {
         cookieCount = cookieCount - ((autoClick+1) * 3);
         autoClick = autoClick + 1;
         update()
@@ -70,10 +93,35 @@ buyGrandmaElm.onclick = function() {
         update();
     }
 }
-/*
+
 //Buy Farm
 var buyFarmElm = document.getElementById('buyFarm');
 var farm = 0;
 buyFarmElm.onclick = function() {
-    if(cookieCount >= )
-}*/
+    if(cookieCount >= (farm+1) * 30) {
+        cookieCount = cookieCount - ((farm+1) * 30);
+        farm = farm + 1;
+        update();
+    }
+}
+//Buy Bakery
+var buyBakeryElm = document.getElementById('buyBakery');
+var bakery = 0;
+buyBakeryElm.onclick = function() {
+    if(cookieCount >= (bakery+1) * 50) {
+        cookieCount = cookieCount - ((bakery+1) * 50);
+        bakery = bakery + 1;
+        update();
+    }
+}
+
+//Buy Mine
+var buyMineElm = document.getElementById('buyMine');
+var mine = 0;
+buyMineElm.onclick = function() {
+    if(cookieCount >= (mine+1) * 80) {
+        cookieCount = cookieCount - ((mine+1) * 80);
+        mine = mine + 1;
+        update();
+    }
+}
